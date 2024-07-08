eedda1f7c853ed71404b2ac1620bfde19a59d382
Kevin Liu <kevin07liu@gmail.com>
2024-07-08 22:31:08 +0800

    feat: reponsive pass on dashboard layout

--- src/routes/(dashboard)/invoices/+page.svelte
+++ src/routes/(dashboard)/invoices/+page.svelte
@@ -1,1 +1,183 @@
 Invoices
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. At amet perferendis beatae. Impedit nemo
+	labore fugiat repellat, nam odit architecto, doloremque quasi aut laudantium vitae saepe facilis
+	illum dolor eum.
+</p>
